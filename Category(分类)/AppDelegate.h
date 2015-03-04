0f65d89963bf7ade0e12458d45c8615fea1300bd
970867019@qq.com <970867019@qq.com>
2015-03-04 23:45:56 +0800

    Initial Commit

---
+++ Category(分类)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Category(分类)
+//
+//  Created by yake on 15/3/4.
+//  Copyright (c) 2015年 SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Category(分类)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Category(分类)
+//
+//  Created by yake on 15/3/4.
+//  Copyright (c) 2015年 SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
